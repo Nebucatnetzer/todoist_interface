dce2ed8282e5b3cc47b5be40142ae120707ee10d
Andreas Zweili <andreas@zweili.ch>
2021-11-06 20:04:11 +0100

    add an assignee to the settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -5,4 +5,5 @@ def test_settings():
     config = settings.read_config("todoist_interface.yml.example")
     assert (config["gitlab"]["token"] == "gitlabtoken"
             and config["gitlab"]["url"] == "https://gitlab.example.com/api/v4/"
-            and config["todoist"]["token"] == "todoisttoken")
+            and config["todoist"]["token"] == "todoisttoken"
+            and config["gitlab"]["assignee"] == "muster")
