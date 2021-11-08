d1d0e42b95d512fd64a6fd377e368886bce188cd
Andreas Zweili <andreas@zweili.ch>
2021-11-08 15:50:16 +0100

    some changes

--- tests/test_gitlab.py
+++ tests/test_gitlab.py
@@ -2,5 +2,5 @@ from todoist_interface.gitlab import GitlabAPI
 
 
 def test_gitlab_init():
-    gitlab = GitlabAPI("url", "token")
+    gitlab = GitlabAPI("url", "token", "assignee")
     assert gitlab.url == "url" and gitlab.token == "token"
