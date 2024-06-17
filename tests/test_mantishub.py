e7ca4c23578d1c369e245702f08de2f480a094e1
Andreas Zweili <andreas@zweili.ch>
2024-06-17 22:09:11 +0200

    Fix tests for mantishub

--- tests/test_mantishub.py
+++ tests/test_mantishub.py
@@ -1,32 +1,31 @@
 import json
 import requests
 
-from todoist_interface import mantishub
-
 import mocks
 
-
-def test_mantishub_init():
-    api = mantishub.MantishubAPI("mantistoken")
-    assert api.token == "mantistoken"
+from todoist_interface import mantishub
 
 
 def test_mantishub_get_tickets(monkeypatch, example_tickets):
 
-    def mock_get(*args, **kwargs):
+    def mock_get(*_, **__):
         return mocks.MockResponse(example_tickets)
 
     # apply the monkeypatch for requests.get to mock_get
     monkeypatch.setattr(requests, "get", mock_get)
-    api = mantishub.MantishubAPI("mantistoken")
-    tickets = api.get_tickets()
-    assert tickets[0]['content'] == "[Sample issue title](https://contria.mantishub.io/view.php?id=1)"
+    tickets = mantishub.get_tickets(token="1234")
+    assert (
+        tickets[0]["content"]
+        == "[Sample issue title](https://contria.mantishub.io/view.php?id=1)"
+    )
 
 
 def test_covert_to_todoist(example_tickets):
     issues = json.loads(example_tickets)
-    tasks = mantishub.convert_to_todoist(issues["issues"])
+    tasks = mantishub._convert_to_todoist(issues["issues"])
     assert tasks == [
-        {'content': '[Sample issue title](https://contria.mantishub.io/view.php?id=1)',
-         'label_ids': [2158784659]}
+        {
+            "content": "[Sample issue title](https://contria.mantishub.io/view.php?id=1)",
+            "label_ids": [2158784659],
+        }
     ]
