030e13b6d7147d96cab87fd77b089f05a9422fd9
Andreas Zweili <andreas@zweili.ch>
2024-06-17 22:13:38 +0200

    Reformat with black

--- tests/test_gitlab.py
+++ tests/test_gitlab.py
@@ -8,17 +8,18 @@ from todoist_interface import gitlab
 
 def test_init():
     api = gitlab.GitlabAPI("url", "token", "assignee")
-    assert (api.url == "url"
-            and api.token == "token"
-            and api.assignee == "assignee")
+    assert api.url == "url" and api.token == "token" and api.assignee == "assignee"
 
 
 def test_covert_to_todoist(example_issues):
     issues = json.loads(example_issues)
     tasks = gitlab.convert_to_todoist(issues)
     assert tasks == [
-        {'content': '[Consequatur vero maxime deserunt laboriosam est voluptas dolorem.](http://gitlab.example.com/my-group/my-project/issues/6)',
-         'label_ids': [2158782094]}]
+        {
+            "content": "[Consequatur vero maxime deserunt laboriosam est voluptas dolorem.](http://gitlab.example.com/my-group/my-project/issues/6)",
+            "label_ids": [2158782094],
+        }
+    ]
 
 
 def test_gitlab_get_tickets(monkeypatch, example_issues):
@@ -30,5 +31,7 @@ def test_gitlab_get_tickets(monkeypatch, example_issues):
     monkeypatch.setattr(requests, "get", mock_get)
     api = gitlab.GitlabAPI("url", "token", "assignee")
     issues = api.get_issues()
-    assert issues[0][
-        'content'] == "[Consequatur vero maxime deserunt laboriosam est voluptas dolorem.](http://gitlab.example.com/my-group/my-project/issues/6)"
+    assert (
+        issues[0]["content"]
+        == "[Consequatur vero maxime deserunt laboriosam est voluptas dolorem.](http://gitlab.example.com/my-group/my-project/issues/6)"
+    )
