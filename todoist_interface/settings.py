172c1ac225370dbd2e5ef3c9483020eb2a1b578b
Andreas Zweili <andreas@zweili.ch>
2024-12-23 17:27:30 +0100

    Add types to settings.py

--- todoist_interface/settings.py
+++ todoist_interface/settings.py
@@ -1,7 +1,9 @@
+from typing import Any
+
 import yaml
 
 
-def read_config(config_path):
+def read_config(config_path: str) -> Any:
     with open(file=config_path, encoding="utf-8", mode="r") as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
         return cfg
