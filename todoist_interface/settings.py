76f612265a209a1058625a59a4142956b438b99d
Andreas Zweili <andreas@zweili.ch>
2024-12-23 17:27:16 +0100

    Rewrite open to kwargs

--- todoist_interface/settings.py
+++ todoist_interface/settings.py
@@ -2,6 +2,6 @@ import yaml
 
 
 def read_config(config_path):
-    with open(config_path, "r") as ymlfile:
+    with open(file=config_path, encoding="utf-8", mode="r") as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
         return cfg
